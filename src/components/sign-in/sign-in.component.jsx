1f9e0b52447ebada928c760041a38ee060454e0d
Ahmad-Jawad <ahmadjawadh@yahoo.com>
2019-12-13 18:47:08 +1300

    sign in page has been added with styling, new components sign-in, sign-in-and-sign-up, form-input and custom-button. Problems: background images are not showing. and two lower components does not catch the styles of 'large'

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+class SignIn extends React.Component{
+    constructor(props){
+        super();
+        
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = event => {
+        event.preventDefault();
+
+        this.setState({email:'', password:''});
+    };
+
+    handleSubmit = event => {
+        const {value, name} = event.target; 
+        this.setState ({[name]: value});
+    };
+
+    render(){
+        return (
+            <div className="sign-in">
+                <h2>Already have and account, Sign in</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput label="Email" handleChange={this.handleChange} name="email" type="email" value={this.state.email} required />
+
+                    <FormInput label="Password" handleChange={this.handleChange} name="password" type="password" value={this.state.password} required />
+
+
+                    <CustomButton type="submit" > Sign in</CustomButton>
+                </form>
+
+            </div>
+        )
+    }
+
+}
+
+export default SignIn;
